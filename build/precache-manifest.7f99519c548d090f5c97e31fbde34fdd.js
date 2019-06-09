99a92ebc162b749f8b2b7bc5b67f3f235d17a5fe
seregakreew <46082951+seregakreew@users.noreply.github.com>
2019-06-09 15:28:02 +0300

    Add files via upload

---
+++ build/precache-manifest.7f99519c548d090f5c97e31fbde34fdd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d96e4fd0887fe4b3af2677ec5339552d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a18f437169be378b2a55",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e083292f352b8a48b197",
+    "url": "/static/js/2.61ebbd48.chunk.js"
+  },
+  {
+    "revision": "a18f437169be378b2a55",
+    "url": "/static/js/main.69f0a535.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
