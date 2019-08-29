21aa7b6137fc60b371e67a7bc3808a3bb4a670c6
seregakreew <46082951+seregakreew@users.noreply.github.com>
2019-08-29 16:24:49 +0300

    Add files via upload

---
+++ vk-mini/precache-manifest.74ee5519d17b06d10b6a62aa96f0650e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "41eb0f0ce8b086180fea",
+    "url": "/vk-mini/static/css/main.8811e007.chunk.css"
+  },
+  {
+    "revision": "41eb0f0ce8b086180fea",
+    "url": "/vk-mini/static/js/main.a61c091a.chunk.js"
+  },
+  {
+    "revision": "ffd47ee3b322dd4164a6",
+    "url": "/vk-mini/static/js/runtime~main.f2c41805.js"
+  },
+  {
+    "revision": "11707b206397edf842ef",
+    "url": "/vk-mini/static/css/2.92821c09.chunk.css"
+  },
+  {
+    "revision": "11707b206397edf842ef",
+    "url": "/vk-mini/static/js/2.b7851279.chunk.js"
+  },
+  {
+    "revision": "4c3c9d50f5601e18a34c12476e2c2f69",
+    "url": "/vk-mini/index.html"
+  }
+];
